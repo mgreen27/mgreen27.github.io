80390a882769641f4efd4d2e644560f3c7e95349
mgreen27 <mgreen27@gmail.com>
2019-11-11 11:28:41 +0000

    Add VRaptor LR Autormation

--- static/other/Velociraptor/VRaptorAPISetup.sh
+++ static/other/Velociraptor/VRaptorAPISetup.sh
@@ -3,14 +3,16 @@
 # Author: @mgreen27
 
 # This script will setup Velociraptor API services
-# Assumes wget installed tested in CentOS only
-# Additional forensic tools required installation
+# Assumes wget installed.
+# This script will not install forensic tools  on ubuntu...
+#
 # Variables:
 #     BINFOLDER - folder where velociraptor installed
 #     CONFIG - folder where server.config.yaml and where to store api_config.yaml
 #     API - API IP and port - localhost:8001 default
 #     FOLDER - API program folder where to install bindings
 #     VUser and VGroup - velociraptor user and group names
+
 
 BINFOLDER=/usr/local/bin
 CONFIG=/etc/velociraptor
@@ -23,66 +25,70 @@ VGROUP=velociraptor
 # Get Operating System
 os=$(sudo cat /etc/os-release | egrep -i "centos|ubuntu" -om 1)
 
+sudo mkdir $FOLDER -p
+
 if [ ${os^^} == "CENTOS" ]; then
     echo -e "\nInstalling CentOS Velociraptor API POC"
-    exit
 
-	# Python3 and API requirements
-	sudo yum -y install python36 python36-pip
-	sudo pip3 install grpcio grpcio-tools pyyml -q
+    # Python3 and API requirements
+    sudo yum -y install python36 python36-pip git
+    sudo pip3 install grpcio grpcio-tools pyyml -q
 
     # Install CERT-tools
     CERT="https://forensics.cert.org/cert-forensics-tools-release-el7.rpm"
-	sudo yum -y install epel-release 
-	sudo yum -y update epel-release
-	sudo yum -y install centos-release-scl-rh
-	wget $CERT
-	sudo rpm -i $(basename $CERT)
-	rm -f $(basename $CERT)
+    sudo yum -y install epel-release 
+    sudo yum -y update epel-release
+    sudo yum -y install centos-release-scl-rh
+    wget $CERT
+    sudo rpm -i $(basename $CERT)
+    rm -f $(basename $CERT)
 
-	# install some tools for POC
-	yum -y install plaso
-	yum -y install analyzeMFT
-	wget -P $BINFOLDER https://raw.githubusercontent.com/PoorBillionaire/USN-Journal-Parser/master/usnparser/usn.py
-	chmod +x $BINFOLDER/usn.py
+    # install some tools for POC
+    yum -y install plaso
+    yum -y install analyzeMFT
+    wget -P $BINFOLDER https://raw.githubusercontent.com/PoorBillionaire/USN-Journal-Parser/master/usnparser/usn.py
+    chmod +x $BINFOLDER/usn.py
 
 elif [ ${os^^} == "UBUNTU" ]; then
-	echo -e "\nPlaceholder - Ubuntu not supported for Velociraptor API POC\n"
-	exit
+    echo -e "\nInstalling CentOS Velociraptor API POC"
+    
+    # Python3 and API requirements
+    sudo apt-get -y install python3 python3-pip git
+    sudo pip3 install grpcio grpcio-tools pyyml -q
 else
     echo -e "\nVelociraptor API POC - Unsupported OS. Exiting.\n"
     exit
 fi
 
 
-# install common components
-sudo mkdir $FOLDER -p
-sudo chown -R ${VUSER}:${VGROUP} $FOLDER
 
+# If no Velocirpator binary/distributed install we need to download for processing evtx
+# download latest release
 if [ ! -f "$BINFOLDER/velociraptor" ]
 then
-	# distributed install so download Velociraptor for processing
-	LINUX="$(curl -s https://api.github.com/repos/Velocidex/velociraptor/releases/latest  | grep browser_download_url | cut -d '"' -f 4 | grep linux-amd64)"
-	wget $LINUX
-	chmod +x $(basename $LINUX)
-	sudo mv $(basename $LINUX) $BINFOLDER/velociraptor
+    # distributed install so download Velociraptor for processing
+    LINUX="$(curl -s https://api.github.com/repos/Velocidex/velociraptor/releases/latest  | grep browser_download_url | cut -d '"' -f 4 | grep linux-amd64)"
+    wget $LINUX
+    chmod +x $(basename $LINUX)
+    sudo mv $(basename $LINUX) $BINFOLDER/velociraptor
 else
-	# Generate API config for local install
-	sudo $BINFOLDER/velociraptor --config $CONFIG/server.config.yaml config api_client --name ServerAPI > $CONFIG/api_client.yaml
-	sudo sed -i "s|api_connection_string: 127.0.0.1:8001|api_connection_string: $API|g" $CONFIG/api_client.yaml
+    # Generate API config for local install
+    sudo $BINFOLDER/velociraptor --config $CONFIG/server.config.yaml config api_client --name ServerAPI > $CONFIG/api_client.yaml
+    sudo sed -i "s|api_connection_string: 127.0.0.1:8001|api_connection_string: $API|g" $CONFIG/api_client.yaml
 fi
 
 
-# download bindings.
-sudo mkdir $FOLDER/api
+# download python bindings
+sudo mkdir $FOLDER
 wget -P $FOLDER https://raw.githubusercontent.com/Velocidex/velociraptor/master/bindings/python/api.proto
 wget -P $FOLDER https://raw.githubusercontent.com/Velocidex/velociraptor/master/bindings/python/api_pb2.py
 wget -P $FOLDER https://raw.githubusercontent.com/Velocidex/velociraptor/master/bindings/python/api_pb2_grpc.py
 sudo touch /usr/local/lib64/python3.6/site-packages/google/__init__.py
 
-# download client
+# download client latest release
 wget -P $FOLDER https://raw.githubusercontent.com/mgreen27/mgreen27.github.io/master/static/other/Velociraptor/processing.py
 wget -P $FOLDER https://github.com/Velocidex/velociraptor/blob/master/bindings/python/client_example.py
+
 
 # build velociraptor processing service
 sudo echo "[Unit]" > /etc/systemd/system/velociraptor-processing.service
@@ -101,8 +107,13 @@ sudo echo "[Install]" >> /etc/systemd/system/velociraptor-processing.service
 sudo echo "WantedBy=multi-user.target" >> /etc/systemd/system/velociraptor-processing.service
 sudo echo "" >> /etc/systemd/system/velociraptor-processing.service
 
+sudo systemctl daemon-reload
+sudo chown -R ${VUSER}:${VGROUP} $FOLDER
+
 # User message
-echo -e "Velociraptor-Processing Service control:"
+echo -e "\n***************************************************"
+echo -e "Velociraptor API POC install complete\n"
+echo -e "\nVelociraptor-Processing Service control:"
 echo -e "\tsudo systemctl status vraptor -l\n"
 echo -e "\tsudo systemctl start vraptor-processing"
 echo -e "\tsudo systemctl stop vraptor-processing"
